c5051d0569264bf7849e8b55ffb330296c023433
Almog Tavor <almogtavor@gmail.com>
2021-10-18 23:32:20 +0300

    fix: inverted index data

--- src/main/java/visualize/lucene/initialize/Utils.java
+++ src/main/java/visualize/lucene/initialize/Utils.java
@@ -1,15 +1,27 @@
 package visualize.lucene.initialize;
 
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.IndexOptions;
 import org.apache.lucene.index.IndexWriter;
 
 import java.io.IOException;
 
 public class Utils {
     public static void addDoc(IndexWriter w, String title, String isbn) throws IOException {
+        Document doc = new Document();
+        FieldType type = new FieldType();
+        type.setStoreTermVectors(true);
+        type.setIndexOptions(IndexOptions.DOCS);
+        // Optional
+        type.setStoreTermVectorPositions(true);
+        type.setStoreTermVectorOffsets(true);
+
+        doc.add(new Field("title", title, type));
+        doc.add(new Field("isbn", isbn, type));
+        w.addDocument(doc);
+    }
+
+    public static void addDocForIndexVis(IndexWriter w, String title, String isbn) throws IOException {
         Document doc = new Document();
         doc.add(new TextField("title", title, Field.Store.YES));
 
